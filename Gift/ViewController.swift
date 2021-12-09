a3822bbf07e8de58bf2908da2a0dcb24d75267a8
Alexis Rojas-Westall <alexis.rojaswestall@gmail.com>
2021-12-08 19:53:40 -0600

    Finished launch screen, basic slider, but haven't done any constraints

--- Gift/ViewController.swift
+++ Gift/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var heatValue: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        heatValue.text = String(0)
     }
 
-
+    @IBAction func SliderValueChanged(_ sender: UISlider) {
+        let currentValue = sender.value
+        if (currentValue == 100) {
+            heatValue.text = String(100)
+        } else {
+            let formattedString = String(format: "%.2f", currentValue)
+            heatValue.text = formattedString
+        }
+        
+        
+    }
+    
 }
 
